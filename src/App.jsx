360ba44616366db2b3019755415b58af7a488980
Carlos <charlyqv98@gmail.com>
2023-12-20 09:05:07 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Aside } from "./components/Aside"
+import { Pokemons } from "./components/Pokemons"
+
+function App() {
+
+  return (
+    <section className="bg-[#F6F8FC] h-screen font-outfit overflow-y-auto">
+      <main className="max-w-[1400px] mx-auto grid grid-cols-1 lg:grid-cols-[1fr_350px]">
+        <Pokemons />
+        <Aside />
+      </main>
+    </section>
+  )
+}
+
+export default App
