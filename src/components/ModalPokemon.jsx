cdfca9b5dfb42d69bf4035c37a0f92d41f692545
Carlos Villaseñor Quintero <charlyqv98@gmail.com>
2023-12-23 12:02:08 -0600

    Avance ModalPokemon

--- src/components/ModalPokemon.jsx
+++ src/components/ModalPokemon.jsx
@@ -1,9 +1,9 @@
 import { IconX } from '@tabler/icons-react'
 import React from 'react'
 
-export const ModalPokemon = () => {
+export const ModalPokemon = ({ showModal }) => {
   return (
-    <section className='fixed top-0 left-0 right-0 bg-green-400 h-screen'>
+    <section className={`fixed top-0 left-0 right-0 bg-green-400 h-screen ${showModal ? "block" : "hidden"}`}>
       <button className='bg-white absolute top-4 right-4 p-1 rounded-lg hover:opacity-80 transition-opacity'>
         <IconX size={32} stroke={4}/>
       </button>
