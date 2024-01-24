30fb5af79f9768ab692cf06e377c06ad4b85ae7b
Carlos <charlyqv98@gmail.com>
2024-01-23 18:46:30 -0600

    FEAT: avance modal del pokemon

--- src/components/ModalPokemon.jsx
+++ src/components/ModalPokemon.jsx
@@ -14,10 +14,9 @@ const colorByStat = {
 }
 
 export const ModalPokemon = ({ showModal, onCloseModal, pokemon }) => {
-  console.log("🚀 ~ ModalPokemon ~ pokemon:", pokemon);
   return (
     <section 
-      className={`fixed top-0 left-0 right-0 bg-green-400 h-screen transition-all duration-500 ${showModal ? "visible opacity-100" : "invisible opacity-0"}`}
+      className={`fixed top-0 left-0 right-0 h-screen transition-all duration-500 ${showModal ? "visible opacity-100" : "invisible opacity-0"} ${colorByType[pokemon?.types[0]]}`}
     >
       <button
         onClick={onCloseModal} 
@@ -26,80 +25,81 @@ export const ModalPokemon = ({ showModal, onCloseModal, pokemon }) => {
         <IconX size={32} stroke={4}/>
       </button>
 
-      <header className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2'>
-        <img src={pokemon?.image} alt="" />
-      </header>
-
       <article 
-        className={`bg-white h-[85%] absolute w-full bottom-0 rounded-tl-3xl rounded-tr-3xl text-center transition-all overflow-auto duration-500 px-4 grid gap-2 content-start ${ showModal ? "bottom-0" : "-bottom-full"}`}
+        className={`bg-white h-[85%] absolute w-full bottom-0 rounded-tl-3xl rounded-tr-3xl text-center transition-all duration-500 ${ showModal ? "bottom-0" : "-bottom-full"}`}
       >
-        
-        <span className='text-slate-400 text-sm font-semibold'>N° {pokemon?.id}</span>
-        <h2 className='font-bold text-2xl capitalize'>{pokemon?.name}</h2>
-        <ul className='flex gap-2 justify-center'>
-          {pokemon?.types.map((type) => (
-            <li 
-              className={`p-1 rounded-md px-2 text-sm ${colorByType[type]}`} 
-              key={type}
-            >
-              {type}
-            </li> 
-          ))}
-        </ul>
-        <div>
-          <h4 className='font-bold capitalize'>Pokedex Entry</h4>
-          <p className='text-slate-400 '>{pokemon?.description}</p>
+
+        <header className='absolute left-1/2 -translate-x-1/2 -translate-y-[92%] z-20 scale-[180%]'>
+          <img className='pixelated' src={pokemon?.image} alt="" />
+        </header>
+        <div className='overflow-y-auto px-4 pt-12 grid gap-2 content-start h-full hidden-scroll'>
+          <span className='text-slate-400 text-sm font-semibold'>N° {pokemon?.id}</span>
+          <h2 className='font-bold text-2xl capitalize'>{pokemon?.name}</h2>
+          <ul className='flex gap-2 justify-center'>
+            {pokemon?.types.map((type) => (
+              <li 
+                className={`p-1 rounded-md px-2 text-sm ${colorByType[type]}`} 
+                key={type}
+              >
+                {type}
+              </li> 
+            ))}
+          </ul>
+          <div>
+            <h4 className='font-bold capitalize'>Pokedex Entry</h4>
+            <p className='text-slate-400 '>{pokemon?.description}</p>
+          </div>
+          {/* Altura y peso */}
+          <section className='grid grid-cols-2 gap-4'>
+            <div className='grid gap-2'>
+              <h4 className='font-bold capitalize'>Height</h4>
+              <span className='bg-slate-100 block rounded-full p-1'>0.7m</span>
+            </div>
+            <div className='grid gap-2'>
+              <h4 className='font-bold capitalize'>Weight</h4>
+              <span className='bg-slate-100 block rounded-full p-1'>6.7kg</span>
+            </div>
+          </section>
+          {/* Habilidades */}
+          <section className='grid gap-2'>
+            <h4 className='font-bold capitalize'>Abilities</h4>
+            <ul className='grid grid-cols-2 gap-4'>
+              {
+                pokemon?.abilities.map(ability => (
+                  <li 
+                    key={ability}
+                    className='bg-slate-100 block rounded-full p-1 capitalize'
+                  >
+                    <span>{ability}</span>
+                  </li>
+                ))
+              }
+            </ul>
+          </section>
+          {/* Stats */}
+          <section className='grid gap-2'>
+            <h4 className='font-bold capitalize'>Stats</h4>
+            <ul className='flex justify-center gap-3 flex-wrap'>
+              {
+                pokemon?.stats.map((stat) => (
+                  <li className={`p-1 rounded-full ${colorByStat[stat.name]}`} key={stat.name}>
+                    <div className='bg-green-500 rounded-full w-[28px] aspect-square grid place-content-center'>
+                      <span className='text-xs text-white font-semibold'>{stat.name}</span>
+                    </div>
+                    <span className='font-semibold text-sm'>{stat.base_stat}</span>
+                  </li>
+                ))
+              }
+            </ul>
+            <ul>
+              <li>Stat</li>
+            </ul>
+          </section>
+          <section className='grid gap-2'>
+            <h4 className='font-bold capitalize'>Evolutions</h4>
+            <Evolutions evolutions={pokemon?.evolutions ?? []}/>
+          </section>
         </div>
-        {/* Altura y peso */}
-        <section className='grid grid-cols-2 gap-4'>
-          <div className='grid gap-2'>
-            <h4 className='font-bold capitalize'>Height</h4>
-            <span className='bg-slate-100 block rounded-full p-1'>0.7m</span>
-          </div>
-          <div className='grid gap-2'>
-            <h4 className='font-bold capitalize'>Weight</h4>
-            <span className='bg-slate-100 block rounded-full p-1'>6.7kg</span>
-          </div>
-        </section>
-        {/* Habilidades */}
-        <section className='grid gap-2'>
-          <h4 className='font-bold capitalize'>Abilities</h4>
-          <ul className='grid grid-cols-2 gap-4'>
-            {
-              pokemon?.abilities.map(ability => (
-                <li 
-                  key={ability}
-                  className='bg-slate-100 block rounded-full p-1 capitalize'
-                >
-                  <span>{ability}</span>
-                </li>
-              ))
-            }
-          </ul>
-        </section>
-        {/* Stats */}
-        <section className='grid gap-2'>
-          <h4 className='font-bold capitalize'>Stats</h4>
-          <ul className='flex justify-center gap-3 flex-wrap'>
-            {
-              pokemon?.stats.map((stat) => (
-                <li className={`p-1 rounded-full ${colorByStat[stat.name]}`} key={stat.name}>
-                  <div className='bg-green-500 rounded-full w-[28px] aspect-square grid place-content-center'>
-                    <span className='text-xs text-white font-semibold'>{stat.name}</span>
-                  </div>
-                  <span className='font-semibold text-sm'>{stat.base_stat}</span>
-                </li>
-              ))
-            }
-          </ul>
-          <ul>
-            <li>Stat</li>
-          </ul>
-        </section>
-        <section className='grid gap-2'>
-          <h4 className='font-bold capitalize'>Evolutions</h4>
-          <Evolutions evolutions={pokemon?.evolutions ?? []}/>
-        </section>
       </article>
     </section>
   )
