133f9762f0bafcb4c5f909240aa3d71c89824d41
Carlos <charlyqv98@gmail.com>
2024-01-23 21:32:09 -0600

    FEAT: imagen de mondo

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const { showDetailPokemon, closePokemonDetail, pokemonDetail, isLoading } = usePokemonContext();
 
   return (
-    <section className="bg-[#F6F8FC] h-screen font-outfit overflow-y-auto">
+    <section className="bg-[#F6F8FC] h-screen font-outfit overflow-y-auto bg-[url(/pokeball-icon.png)] bg-no-repeat bg-[-10%_-20%]">
       <main className="max-w-[1400px] mx-auto grid grid-cols-1 lg:grid-cols-[1fr_350px]">
         <Pokemons />
         <Aside pokemon={pokemonDetail} isLoading={isLoading} />
